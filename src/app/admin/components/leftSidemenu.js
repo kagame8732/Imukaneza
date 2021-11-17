258e89eb06271895337ba9cc35c9d3c187496199
hirwaaldo1 <hirwaaldo1@gmail.com>
2021-11-17 19:35:52 +0200

    it is not good but it will be better

---
+++ src/app/admin/components/leftSidemenu.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Logo from '../../../assets/img/logo.png'
+import Navigation from "./navigation";
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import RoomServiceRoundedIcon from '@material-ui/icons/RoomServiceRounded';
+import ForumRoundedIcon from '@material-ui/icons/ForumRounded';
+import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
+import AssignmentRoundedIcon from '@material-ui/icons/AssignmentRounded';
+import img_doc from '../../../assets/img/documents.svg'
+const LeftSideMenu =(props) =>{
+    const navigations =[
+        {
+            name:"Overview",
+            icon:HomeRoundedIcon,
+        },
+        {
+            name:"Service",
+            icon:RoomServiceRoundedIcon,
+        },
+        {
+            name:"Messages",
+            icon:ForumRoundedIcon,
+        },
+        {
+            name:"Profile",
+            icon:PersonRoundedIcon,
+        },
+        {
+            name:"Documents",
+            icon:AssignmentRoundedIcon,
+        }
+    ]
+    return (
+        <div className="m_r_l">
+            <img src={Logo} alt="" id="left_side_admin_logo"/>
+            <p id="name_web_left_side">Creators Agency</p>
+            {
+                navigations.map((item,key)=>{
+                    return <Navigation name={item.name} theOne={props.whichOne} icon={item.icon} key={key}/>
+                })
+            }
+            <img src={img_doc} id="admin_img_style" alt=""/>
+            <p id="report_style">PDF Report</p>
+            <p id="report_details_style">Annual detailed report</p>
+            <button id="download_button_admin">Download</button>
+        </div>
+    )
+}
+export default LeftSideMenu;
---
+++ src/app/admin/widgets/pay.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Payimg from "../../../assets/img/payImg.PNG"
+const PayCard =(porps) =>{
+    return (
+      <div>
+          <div className="row col-12" id="pay_card">
+                <div className="col-sm noM">
+                    <img src={Payimg}  alt="" id="payImg"/>
+                </div>
+                <div className="col-sm noM" id="sas">
+                    <p  id="paywords_style">Pay</p>
+                </div>
+                <p id="code_card">**** **** **** **** 5601</p>
+                <div className="row col-12" id="valid_card">
+                    <div className="col-sm noM">
+                        <p id="up"className="noM">Valid THRU</p>
+                        <p id="down"className="noM">05/21</p>
+                    </div>
+                    <div className="col-sm noM">
+                        <p id="up" className="noM">Card Holder</p>
+                        <p id="down" className="noM">05/21</p>
+                    </div>
+                </div>
+          </div>
+     
+      </div>
+    )
+}
+export default PayCard;
